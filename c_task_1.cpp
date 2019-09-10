2636f210baac435c794e0b50d717cf2725cc0cc4
Disha200 <55154895+Disha200@users.noreply.github.com>
2019-09-10 23:59:16 +0530

    Add files via upload

---
+++ c_task_1.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+int main () {
+char name[50];
+int age;
+int phone[10];
+ 
+   printf("enter name \n");
+   scanf("%c", &name);  
+   printf("enter age \n");    
+   scanf("%d", &age);
+   printf("enter phone \n");    
+   scanf("%d", &phone);  
+    
+    getch();
+    
+    
+    
+    
+}
