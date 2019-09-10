19f71b268f33c1da9443a3e62c5dc186ee68be88
Disha200 <55154895+Disha200@users.noreply.github.com>
2019-09-11 00:14:36 +0530

    Update c_task_2.c

--- c_task_2.c
+++ c_task_2.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
  int main() { 
-    float celsius, fahrenheit;  printf("Enter temperature in Celsius: ");
- scanf("%f", &celsius);  fahrenheit = (celsius * 9 / 5) + 32; 
+    float celsius, fahrenheit; 
+ printf("Enter temperature in Celsius: ");
+ scanf("%f", &celsius); 
+ fahrenheit = (celsius * 9 / 5) + 32; 
 printf("%f Celsius = %f Fahrenheit", celsius, fahrenheit);
  return 0;
      }
